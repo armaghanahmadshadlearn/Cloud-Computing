2e1b86d69f6bba47b5cf450d272744b112cd7e92
armaghanahmadshadlearn <armaghanlab26@gmail.com>
2026-05-06 14:15:38 +0500

    Change items in lists

---
+++ changelist.py
@@ -0,0 +1,9 @@
+list1=["Ahmad","Haider","Hassan","Talha","Ammar"]
+# change item value
+list1[4]="Hamid"
+print(list1)
+# Change a Range of Item Values
+list1[1:5]=["Rabia","Iram","Dua","Areeba"]
+print(list1)
+list1.insert(1,"Talha")
+print(list1)
