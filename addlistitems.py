c67d78920816ad879cc6f2be18bfb1b615ad9d81
armaghanahmadshadlearn <armaghanlab26@gmail.com>
2026-05-06 14:30:05 +0500

    Adding items inside the list

---
+++ addlistitems.py
@@ -0,0 +1,11 @@
+# Append Items
+list1=["Ahmad","Haider","Hassan","Talha","Ammar"]
+list1.append("Armaghan")
+print(list1)
+# Insert Items
+list1.insert(0,"Shad")
+print(list1)
+# Extend List
+list2=["Rabia","Iram","Dua","Areeba"]
+list1.extend(list2)
+print(list1)
