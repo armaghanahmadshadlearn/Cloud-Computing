31d146fb6f0de9cebc97587f2783261aa1362930
armaghanahmadshadlearn <armaghanlab26@gmail.com>
2026-05-14 15:00:17 +0500

    Check wether number is even odd or negative positive

---
+++ check number nature.py
@@ -0,0 +1,12 @@
+a=int(input("Enter the number:"))
+if a%2==0:
+    print("The number is even")
+else:
+    print("The number is odd")
+
+if a>0:
+    print("The number is positive")
+elif a<0:
+    print("The number is negative")
+else:
+    print("The number is zero")
